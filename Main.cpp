890582605c1cab0934395661b800dd635bccdb41
dia_maisun <dia_maisun@jcom.home.ne.jp>
2018-12-14 07:50:19 +0900

    .

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,31 @@
 
 
 
-#include	<cstdio>
-#include	<vector>
-#include	<thread>
+#include    <cstdio>
+#include    <vector>
+#include    <thread>
 
 
 
 struct Test {
-	~Test()
-	{
-		std::printf("~Test %p\n", this); fflush(stdout);
-		
-		// "Msvc" will stop. That is correct.
-		// "g++" does not stop. That is dangerous.
-		*static_cast<int*>(nullptr) = 0;
-	}
-	
-	Test()
-	{
-		std::printf("Test %p\n", this); fflush(stdout);
-	}
-	
-	void Func()
-	{
-		std::printf("Func %p\n", this); fflush(stdout);
-	}
+    ~Test()
+    {
+        std::printf("~Test %p\n", this); fflush(stdout);
+        
+        // "Msvc" will stop. That is correct.
+        // "g++" does not stop. That is dangerous.
+        *static_cast<int*>(nullptr) = 0;
+    }
+    
+    Test()
+    {
+        std::printf("Test %p\n", this); fflush(stdout);
+    }
+    
+    void Func()
+    {
+        std::printf("Func %p\n", this); fflush(stdout);
+    }
 };
 
 thread_local Test v;
@@ -34,8 +34,8 @@ thread_local Test v;
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::thread> at(std::thread::hardware_concurrency());
-	for (auto& t : at) t = std::thread([]{ v.Func(); });
-	for (auto& t : at) t.join();
-	return 0;
+    std::vector<std::thread> at(std::thread::hardware_concurrency());
+    for (auto& t : at) t = std::thread([]{ v.Func(); });
+    for (auto& t : at) t.join();
+    return 0;
 }
